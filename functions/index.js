d2f6843c018dd89e4ac4e2bfc1acdd43b973bad1
erickosanchezj <erickosanchezj@gmail.com>
2025-09-04 20:55:10 -0600

    feat: remind of classes starting soon

--- functions/index.js
+++ functions/index.js
@@ -99,3 +99,6 @@ exports.backfillEmailLower = onCall({ region: "us-central1" }, async (request) =
 const bookingReminders = require("./src/bookingReminders");
 exports.onBookingCreate = bookingReminders.onBookingCreate;
 exports.sendBookingReminder = bookingReminders.sendBookingReminder;
+
+const upcomingClassesReminder = require("./src/upcomingClassesReminder");
+exports.remindUpcomingClasses = upcomingClassesReminder.remindUpcomingClasses;
